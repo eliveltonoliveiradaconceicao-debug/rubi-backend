d04882596113cbef55a0cf689d374220880b89d5
eliveltonoliveiradaconceicao-debug <eliveltonoliveiradaconceicao@gmail.com>
2026-02-20 11:26:52 -0300

    server.js

--- server.js
+++ server.js
@@ -41,10 +41,6 @@ app.post("/create-subscription", async (req, res) => {
   }
 });
 
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -144,4 +140,5 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
